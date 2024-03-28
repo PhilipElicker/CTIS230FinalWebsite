964446c2dc70f1300237a35d4d766b052a10d7e0
PhilipElicker <pelicker@guilford.edu>
2024-03-28 17:45:37 -0400

    Done

--- MainJava.js
+++ MainJava.js
@@ -9,11 +9,10 @@ const output4 = document.getElementById("sliderValue4");
 const slider5 = document.getElementById("mySlider5");
 const output5 = document.getElementById("sliderValue5");
 const output6 = document.getElementById("compositeScore");
-var score1 = 5;
-var score2 = 5;
-var score3 = 5;
-var score4 = 5;
-var score5 = 5;
+const output7 = document.getElementById("letterScore");
+
+var totalScore = 25;
+var letter = "C";
 
 // Display the default slider value
 output.innerHTML = slider.value;
@@ -22,29 +21,49 @@ output3.innerHTML = slider3.value;
 output4.innerHTML = slider4.value;
 output5.innerHTML = slider5.value;
 
+function updateScore() {
+    totalScore = parseInt(slider.value) + parseInt(slider2.value) + parseInt(slider3.value) + parseInt(slider4.value) + parseInt(slider5.value);
+    if (totalScore >= 40) {
+        letter = "A";
+    } else if (totalScore >= 30) {
+        letter = "B";
+    } else if (totalScore >= 20) {
+        letter = "C";
+    } else if (totalScore >= 10) {
+        letter = "D";
+    } else {
+        letter = "F";
+    }
+};
+
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-  score1 = this.value;
-  output6.innerHTML = (score1 + score2 + score3 + score4 + score5)/10000;
+  updateScore();
+  output6.innerHTML = totalScore;
+  output7.innerHTML = letter;
 };
 slider2.oninput = function() {
     output2.innerHTML = this.value;
-    score2 = this.value;
-  output6.innerHTML = (score1 + score2 + score3 + score4 + score5)/10000;
+    updateScore();
+  output6.innerHTML = totalScore;
+  output7.innerHTML = letter;
 };
 slider3.oninput = function() {
     output3.innerHTML = this.value;
-    score3 = this.value;
-  output6.innerHTML = (score1 + score2 + score3 + score4 + score5)/10000;
+    updateScore();
+  output6.innerHTML = totalScore;
+  output7.innerHTML = letter;
 };
 slider4.oninput = function() {
     output4.innerHTML = this.value;
-    score4 = this.value;
-  output6.innerHTML = (score1 + score2 + score3 + score4 + score5)/10000;
+    updateScore();
+  output6.innerHTML = totalScore;
+  output7.innerHTML = letter;
 };
 slider5.oninput = function() {
     output5.innerHTML = this.value;
-    score5 = this.value;
-  output6.innerHTML = (score1 + score2 + score3 + score4 + score5)/10000;
+    updateScore();
+  output6.innerHTML = totalScore;
+  output7.innerHTML = letter;
 };
